adf892d1804d711c5d27eeeafd9b9458e93a2d2b
Aviv <shabtay.aviv@gmail.com>
2024-12-07 23:35:04 +0200

    Added more test for the new utilities & used asserts
    * chore: Fixed char[] & wchar_t[] asserts

--- StringUtils/Main.cpp
+++ StringUtils/Main.cpp
@@ -1,38 +1,70 @@
+#include <cassert>
+
 #include "StringUtils.h"
 
 #include <string_view>
 #include <string>
 #include <iostream>
+#include <optional>
 
 using namespace std::literals;
-using namespace stringUtils;
 
 int main()
 {
-    std::wcout << toWide(std::string("aviv")) << std::endl;
-    std::cout << toMultiByte(std::wstring(L"aviv")) << std::endl;
-    std::wcout << toWide("aviv"sv) << std::endl;
-    std::cout << toMultiByte(L"aviv"sv) << std::endl;
-    std::wcout << toWide("aviv") << std::endl;
-    std::cout << toMultiByte(L"aviv") << std::endl;
+    // char[]
+    constexpr char data[] = "Hello";
+    static_assert(5 == stringUtils::stringLength(data));
+    static_assert(5 == stringUtils::stringSize(data));
 
-    std::string nameUpper("AVIV");
-    std::cout << toLower(nameUpper) << std::endl;
-    std::cout << toLower("AVIV"sv) << std::endl;
-    std::wstring nameUpperWide(L"AVIV");
-    std::wcout << toLower(nameUpperWide) << std::endl;
-    std::wcout << toLower(L"AVIV"sv) << std::endl;
-    std::cout << toLower("AVIV") << std::endl;
-    std::wcout << toLower(L"AVIV") << std::endl;
+    // char*
+    assert(5 == stringUtils::stringLength(&data[0]));
+    assert(5 == stringUtils::stringSize(&data[0]));
 
-    std::string nameLower("aviv");
-    std::cout << toUpper(nameLower) << std::endl;
-    std::cout << toUpper("aviv"sv) << std::endl;
-    std::wstring nameLowerWide(L"aviv");
-    std::wcout << toUpper(nameLowerWide) << std::endl;
-    std::wcout << toUpper(L"aviv"sv) << std::endl;
-    std::cout << toUpper("aviv") << std::endl;
-    std::wcout << toUpper(L"aviv") << std::endl;
+    // wchar_t[]
+    constexpr wchar_t wideData[] = L"Hello";
+    static_assert(5 == stringUtils::stringLength(wideData));
+    static_assert(10 == stringUtils::stringSize(wideData));
+
+    // wchar_t*
+    assert(5 == stringUtils::stringLength(&wideData[0]));
+    assert(10 == stringUtils::stringSize(&wideData[0]));
+
+    // std::string
+    assert(5 == stringUtils::stringLength("hello"s));
+    assert(5 == stringUtils::stringSize("hello"s));
+
+    // std::wstring
+    assert(5 == stringUtils::stringLength(L"hello"s));
+    assert(10 == stringUtils::stringSize(L"hello"s));
+
+    // std::string_view
+    assert(5 == stringUtils::stringLength("hello"sv));
+    assert(5 == stringUtils::stringSize("hello"sv));
+
+    // std::wstring_view
+    assert(5 == stringUtils::stringLength(L"hello"sv));
+    assert(10 == stringUtils::stringSize(L"hello"sv));
+
+    assert(L"aviv" == stringUtils::toWide("aviv"s));
+    assert("aviv" == stringUtils::toMultiByte(L"aviv"s));
+    assert(L"aviv" == stringUtils::toWide("aviv"sv));
+    assert("aviv" == stringUtils::toMultiByte(L"aviv"sv));
+    assert(L"aviv" == stringUtils::toWide("aviv"));
+    assert("aviv" == stringUtils::toMultiByte(L"aviv"));
+
+    assert("aviv" == stringUtils::toLower("AVIV"s));
+    assert("aviv" == stringUtils::toLower("AVIV"sv));
+    assert(L"aviv" == stringUtils::toLower(L"AVIV"s));
+    assert(L"aviv" == stringUtils::toLower(L"AVIV"sv));
+    //assert("aviv" == stringUtils::toLower("AVIV"));
+    //assert(L"aviv" == stringUtils::toLower(L"AVIV"));
+
+    assert("AVIV" == stringUtils::toUpper("aviv"s));
+    assert("AVIV" == stringUtils::toUpper("aviv"sv));
+    //assert(L"AVIV" == stringUtils::toUpper(L"aviv"));
+    assert(L"AVIV" == stringUtils::toUpper(L"aviv"sv));
+    //assert("AVIV" == stringUtils::toUpper("aviv"));
+    //assert(L"AVIV" == stringUtils::toUpper(L"aviv"));
 
     return 0;
 }
